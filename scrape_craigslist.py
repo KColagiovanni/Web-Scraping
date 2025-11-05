a093387244d60d55f9613b7d2bb9213890fdaac0
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2025-11-04 22:51:14 -0800

    Now someothing happened and none of the ads are showing up inthe console results. :-(

--- scrape_craigslist.py
+++ scrape_craigslist.py
@@ -4,11 +4,14 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import time
+from urllib.parse import urljoin
 
 def search_cl(keyword):
 
     count = 0
-    search_url = f'https://sfbay.craigslist.org/search/cta?query={keyword}#search=2~gallery~0'
+    base_url = "https://sfbay.craigslist.org"
+    search_url = f"{base_url}/search/cta?query={keyword}=2~gallery~0"
+    # search_url = f'https://sfbay.craigslist.org/search/cta?query={keyword}#search=2~gallery~0'
 
     options = Options()
     options.add_argument("--headless")  # run in background
@@ -22,7 +25,7 @@ def search_cl(keyword):
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify()[:3000])
+    # print(soup.prettify()[:3000])
     json_script = soup.find('script', {'id': 'ld_searchpage_results', 'type': 'application/ld+json'})
 
     if json_script:
@@ -52,9 +55,25 @@ def search_cl(keyword):
             )
             link = post_links[i] if i < len(post_links) else "N/A"
 
-            if keyword in name:
-                count += 1
-                print(f"{count}. {name} | ${price} | {location} | {link}")
+            # if keyword in name:
+            #     count += 1
+            print(f"{count}. {name} | ${price} | {location} | {link}")
+
+        # print(f'\n{keyword} Results:')
+        # for i, li in enumerate(items, start=1):
+        #     title_tag = li.select_one("a.cl-app-anchor") or li.select_one("a")
+        #     price_tag = li.select_one("span.price")
+        #     location_tag = li.select_one("span.nearby, span.location")
+        #
+        #     title = title_tag.get_text(strip=True) if title_tag else "N/A"
+        #     href = title_tag.get("href") if title_tag and title_tag.has_attr("href") else None
+        #     price = price_tag.get_text(strip=True) if price_tag else "N/A"
+        #     location = location_tag.get_text(strip=True) if location_tag else "N/A"
+        #
+        #     link = urljoin(base_url, href) if href else "N/A"
+        #
+        #     print(f"{i}. {title} | {price} | {location} | {link}")
+
     else:
         print("Could not find JSON data.")
 
